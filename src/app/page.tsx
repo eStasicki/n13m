95bfb0050810fe6052b1a0f18f4d4fc4c088ed51
Ernest Stasicki <estasicki@gmail.com>
2023-09-06 00:02:25 +0200

    chore: add container into home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Container } from "../app/components/atoms/container";
+
 export default function Home() {
 	return (
-		<main>
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo repudiandae sapiente enim
-			unde possimus atque corrupti! Repudiandae non exercitationem obcaecati! Aliquam optio quod
-			iste magni? Alias modi aperiam ullam tenetur?
-		</main>
+		<Container>
+			<main>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo repudiandae sapiente
+				enim unde possimus atque corrupti! Repudiandae non exercitationem obcaecati! Aliquam optio
+				quod iste magni? Alias modi aperiam ullam tenetur?
+			</main>
+		</Container>
 	);
 }
